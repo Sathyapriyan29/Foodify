7db9531898ee9d1014f42b597871f72a5cbf52f1
Sathyapriyan29 <sathyapriyan2261@gmail.com>
2025-01-05 12:48:57 +0530

    first-commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React, { useContext, useState } from 'react'
+import "./Navbar.css"
+import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom'
+import { StoreContext } from '../../context/StoreContext'
+const Navbar = ({setShowLogin}) => {
+
+  const {getTotalCartAmount} = useContext(StoreContext);
+    
+  return (
+    <div className='navbar'>
+        <div>
+        <h1>Foodify</h1>
+        <p>Fresh - Fast</p>
+        </div>
+        <ul className="navbar-menu">
+            <Link to="/"  className='nav-hover'>Home</Link>
+            <a href='#explore-menu'    className='nav-hover'>Menu</a>
+            <a href='#app-download'  className='nav-hover'>Mobile-app</a>
+            <a href='#footer'  className='nav-hover'>Contact us</a>
+        </ul>
+        <div className="navbar-right">
+           
+         <img className='icon' src={assets.search_icon} alt="" />
+            <div className="navbar-search-icon">
+            <Link to="cart" ><img className='icon-basket' src={assets.basket_icon} alt="" /></Link> 
+                <div className={getTotalCartAmount()===0?"":"dot"}></div>
+            </div>
+            <button onClick={()=>setShowLogin(true)} >Sign In</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
